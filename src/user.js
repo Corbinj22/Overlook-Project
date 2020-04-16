54ae3305962a91bb99814fb19e408b427226c5b2
Justin Corbin <jcorbin2290@gmail.com>
2020-04-16 16:40:28 -0600

    Corrected spelling

--- src/user.js
+++ src/user.js
@@ -1,21 +1,24 @@
 class User {
-  constuctor(name, id) {
+  constructor(name, id) {
     this.name = name;
     this.id = id;
-    this.bookings = this.getFutureBookings(id);
-    this.pastBookings = this.getPastBookings(id);
-    this.totalSpent = this.getTotalSpent(id);
+    // this.bookings = this.getFutureBookings(id);
+    // this.pastBookings = this.getPastBookings(id);
+    // this.totalSpent = this.getTotalSpent(id);
   }
   getFutureBookings(id) {
     //iteratre through bookings data and match all bookings by id from today on
+    this.bookings = 3;
   }
 
   getPastBookings(id) {
-    //iteratre through bookings data and match all yesterady and beyond
+  //iteratre through bookings data and match all yesterady and beyond
+  this.pastBookings = 3;
   }
 
   getTotalSpent() {
     //itterate through all bookings match value of rooms and return the value
+    this.totalSpent = 3;
   }
 }
 
