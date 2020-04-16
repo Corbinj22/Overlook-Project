9f5b12a189e1bdab02b528c828e7f7daf3228a3e
Justin Corbin <jcorbin2290@gmail.com>
2020-04-16 16:40:17 -0600

    Changed changeLoadPage to handle more dom manipulation

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import './css/base.scss';
 import Fetcher from './Fetcher';
 import  User from './user';
 import Manger from './manager';
-import domUpdates from './domUpdates';
+import domUpdates from './domUpdates.js'
 
 const fetcher = new Fetcher()
 let userData;
 let bookingData;
 let roomsData;
-// let currentUser;
+let currentUser;
 
 function getAllData() {
   let fetchedUserData = fetcher.fetchUserData()
-  .then(data => data);
+    .then(data => data);
 
   let fetchedBookingsData = fetcher.fetchBookingsData()
     .then(data => data);
@@ -30,18 +30,34 @@ function setData(data) {
   roomsData = data[2].rooms;
 }
 
-//global test eventListener////
-// document.querySelector('body').addEventListener('click',() => {
-//   console.log(roomsData)
-// })
-//////
-
 $('.login-box').keyup(function() {
   if ($('#login-Name').val().length >= 1 && $('#login-Password').val().length >= 1) {
-    $('#login-image').toggleClass("disabled")
+    $('#login-image').removeClass("disabled")
   } else {
     $('#login-image').addClass("disabled")
   }
 })
+
+function changeLoadPage(userName, userPassword) {
+  var userId = parseInt(userName.match(/\d+/));
+  if (userName === 'manager' && userPassword === 'overlook2020') {
+    console.log(1);
+
+  } else if (userId <= 50 && userName.includes('customer') && userPassword === 'overlook2020') {
+    let currentUserData = userData[userId]
+    currentUser = new User(currentUserData.name, currentUserData.id)
+  } else {
+    $('#error-login-text').text('Please enter a valid Username an Password')
+  }
+}
+
+$('#login-image').click(function() {
+  if (!$(this).hasClass('disabled')) {
+    var userName = $('#login-Name').val()
+    var userPassword = $('#login-Password').val()
+  }
+  changeLoadPage(userName, userPassword)
+})
+
 
 getAllData().then(data => setData(data))
