53b0b71625fb197c403ae70689d185e5e15ec59d
Justin Corbin <jcorbin2290@gmail.com>
2020-04-16 16:39:35 -0600

    Changed method to live on scripts class

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,14 +1,9 @@
 import $ from 'jquery'
 
-const domupdates = {
-  changeLoadPage(userName, password, number) {
-    if (userName === 'manager' && password === 'overlook2020') {
-      console.log(1);
-    } else if (userName === 'customer' && password === 'overlook2020') {
-      console.log(2)
-    }
-  }
+const domUpdates = {
 
 
 
 }
+
+export default domUpdates;
