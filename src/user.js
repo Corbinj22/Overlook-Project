3408f6b6e02b79316632ea399cb13fed305a4a46
Justin Corbin <jcorbin2290@gmail.com>
2020-04-17 12:27:22 -0600

    Changed methods to allow for verification of future and past bookings

--- src/user.js
+++ src/user.js
@@ -1,25 +1,38 @@
+const moment = require('moment')
+
 class User {
   constructor(name, id) {
     this.name = name;
     this.id = id;
-    // this.bookings = this.getFutureBookings(id);
-    // this.pastBookings = this.getPastBookings(id);
-    // this.totalSpent = this.getTotalSpent(id);
+    this.futureBookings = [];
+    this.pastBookings = [];
+    this.totalSpent = 0;
   }
-  getFutureBookings(id) {
-    //iteratre through bookings data and match all bookings by id from today on
-    this.bookings = 3;
-  }
-
-  getPastBookings(id) {
-  //iteratre through bookings data and match all yesterady and beyond
-  this.pastBookings = 3;
+  getFutureBookings(bookingData) {
+    let todayDate = moment().format('YYYY/MM/DD')
+    this.futureBookings = bookingData
+      .filter(booking => booking.userID === this.id)
+      .filter(booking => booking.date > todayDate)
   }
 
-  getTotalSpent() {
-    //itterate through all bookings match value of rooms and return the value
-    this.totalSpent = 3;
+  getPastBookings(bookingData) {
+    let todayDate = moment().format('YYYY/MM/DD')
+    this.pastBookings = bookingData
+      .filter(booking => booking.userID === this.id)
+      .filter(booking => booking.date < todayDate)
   }
+
+  getTotalSpent(bookingData, roomsData) {
+    let allUserBookings = bookingData.filter(booking => booking.userID === this.id)
+    let currentUserTotal = allUserBookings.reduce((acc, userBooking) => {
+      let roomValue = roomsData.find(room => room.number === userBooking.roomNumber).costPerNight
+      acc += roomValue
+      return acc;
+    }, 0)
+    this.totalSpent = currentUserTotal;
+  }
+
+
 }
 
 export default User;
