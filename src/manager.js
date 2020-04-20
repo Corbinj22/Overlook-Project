4cff6fec72039afdccca733e1fe74dd737c84e78
Justin Corbin <jcorbin2290@gmail.com>
2020-04-20 10:54:23 -0600

    Created funcionality for manger to see future and past booking of guest

--- src/manager.js
+++ src/manager.js
@@ -1,28 +1,29 @@
+import domUpdates from './domUpdates.js'
 
 class Manager {
   constructor() {
-
+    this.dailyRevenue = 0;
   }
-
   getDailyRevenue(bookingData, currentDate, roomsData) {
     let allDailyBookings = bookingData.filter(booking => booking.date === currentDate)
     let rooms = allDailyBookings.map(booking => roomsData.find(room => room.number === booking.roomNumber))
     let totalRevenue = rooms.reduce((acc, room) => {
       return acc += room.costPerNight;
     }, 0).toFixed(2)
+    this.dailyRevenue = totalRevenue;
     return totalRevenue
   }
 
-  // getDailyAvailableRooms(bookingData, currentDate, roomsData) {
-  //   let formatedDate = requestedDate.split('-').join('/')
-  //   let bookingsPerDay = bookingData
-  //     .filter(booking => booking.date === formatedDate)
-  //     .map(room => room.roomNumber)
-  //   let availableRooms = roomsData.filter(room => {
-  //     if (!bookingsPerDay.includes(room.number)) {
-  //       return room;
-  //     }
-  // }
+  getUserFutureBookings(foundUser, todayDate, bookingData) {
+    let usersPastBookings = bookingData
+      .filter(booking => booking.userID === foundUser.id)
+      .filter(booking => booking.date < todayDate)
+    return usersPastBookings;
+  }
+
+  getUserPastBookings(foundUser) {
+
+  }
 }
 
 export default Manager;
