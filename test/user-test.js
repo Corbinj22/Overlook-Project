b465fa0d2fc8ff38df04330d8065260d26477691
Justin Corbin <jcorbin2290@gmail.com>
2020-04-20 20:37:05 -0600

    Deleted white space

--- test/user-test.js
+++ test/user-test.js
@@ -15,13 +15,13 @@ describe('User', function() {
       pastBookings: [],
       totalSpent: 0
     });
+
     bookingData = [{"id":"5fwrgu4i7k55hl6sz","userID":9,"date":"2020/02/04","roomNumber":15,"roomServiceCharges":[]},
     {"id":"5fwrgu4i7k55hl6t5","userID":10,"date":"2020/07/24","roomNumber":24,"roomServiceCharges":[]},
     {"id":"5fwrgu4i7k55hl6t6","userID":13,"date":"2020/01/10","roomNumber":12,"roomServiceCharges":[]},
     {"id":"5fwrgu4i7k55hl6t7","userID":10,"date":"2020/02/16","roomNumber":7,"roomServiceCharges":[]},
     {"id":"5fwrgu4i7k55hl6t8","userID":1,"date":"2020/02/05","roomNumber":12,"roomServiceCharges":[]},
     {"id":"5fwrgu4i7k55hl6t9","userID":10,"date":"2020/02/14","roomNumber":14,"roomServiceCharges":[]}]
-
 
     roomsData = [{"number":1,"roomType":"residential suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":358.4},
     {"number":2,"roomType":"suite","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":477.38},
